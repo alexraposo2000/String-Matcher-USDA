6855ea3c6f8edd4b8fc8d3ac8ffb2d691a1a4db0
alexraposo2000 <131415043+alexraposo2000@users.noreply.github.com>
2023-07-19 12:30:03 -0400

    Add files via upload

--- BERT_match.py
+++ BERT_match.py
@@ -1,9 +1,8 @@
-from transformers import BertTokenizer, BertModel
-import torch
+# This code is primarily taken from this article: https://towardsdatascience.com/bert-for-measuring-text-similarity-eec91c6bf9e1
+from clean import *
 import pandas as pd
-import numpy as np
-from clean import * # import the cleaner function from clean.py
-
+from transformers import AutoTokenizer, AutoModel
+import torch
 def bert_match(list1, list2, type, join_punct = ', '):
     list1 = cleaner(list1,type,join_punct)
     list2 = cleaner(list2,type,join_punct)
@@ -11,59 +10,62 @@ def bert_match(list1, list2, type, join_punct = ', '):
     if type == 'lists':
         list1 = [', '.join(l1) for l1 in list1]
         list2 = [', '.join(l2) for l2 in list2]
-    # Load pre-trained BERT tokenizer and model
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertModel.from_pretrained('bert-base-uncased')
-    cos = []
-    doc = []
-    df_matches_data = []
+
+    tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+    model = AutoModel.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+
+    sentences = list1+list2
+
+    # initialize dictionary to store tokenized sentences
+    tokens = {'input_ids': [], 'attention_mask': []}
+
+    for sentence in sentences:
+        # encode each sentence and append to dictionary
+        new_tokens = tokenizer.encode_plus(sentence, max_length=128,
+                                           truncation=True, padding='max_length',
+                                           return_tensors='pt')
+        tokens['input_ids'].append(new_tokens['input_ids'][0])
+        tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+
+    # reformat list of tensors into single tensor
+    tokens['input_ids'] = torch.stack(tokens['input_ids'])
+    tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+
+    outputs = model(**tokens)
+    embeddings = outputs.last_hidden_state
+    # ------------------------------------------------------
+    attention_mask = tokens['attention_mask']
+    mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+    masked_embeddings = embeddings * mask
+    summed = torch.sum(masked_embeddings, 1)
+    summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+    mean_pooled = summed / summed_mask
+
+    from sklearn.metrics.pairwise import cosine_similarity
+    # convert from PyTorch tensor to numpy array
+    mean_pooled = mean_pooled.detach().numpy()
+    # calculate similarities and return top 3
     df_scores_data = []
-    for query1 in list1:
-        scores = []
-        for query2 in list2:
-            # Tokenize the sentences
-            tokens = tokenizer.encode_plus(query1, query2, padding=True, truncation=True, return_tensors='pt')
-            print(tokens)
-            # Retrieve token IDs and attention mask
-            input_ids = tokens['input_ids']
-            attention_mask = tokens['attention_mask']
-
-            # Forward pass through BERT model
-            with torch.no_grad():
-                outputs = model(input_ids, attention_mask=attention_mask)
-
-            # Extract the embeddings
-            sentence1_embedding = outputs.last_hidden_state[0][0]  # Embedding for the first sentence
-            sentence2_embedding = outputs.last_hidden_state[0][1]  # Embedding for the second sentence
-
-            # Calculate cosine similarity between sentence embeddings
-            cosine_similarity = torch.nn.functional.cosine_similarity(sentence1_embedding, sentence2_embedding, dim=0)
-            cs = cosine_similarity.item()
-            print('score',cs)
-            scores.append(cs)
-
-        # scores = np.array(scores)
-        print('SCORES', scores)
-        scores_copy = scores.copy()
-        # max_value = max(scores_copy)
-        list2_copy = list2.copy()
-        top_3_matches = []
+    df_matches_data = []
+    for i in range(len(list1)):
         top_3_scores = []
-        for i in range(3):
-            if len(scores_copy)>0:
-                max_idx = scores_copy.index(max(scores_copy))
-                top_3_matches.append(list2_copy[max_idx])
-                top_3_scores.append(max(scores_copy))
-                print(scores_copy[max_idx],list2_copy[max_idx])
-                del scores_copy[max_idx]
+        top_3_matches = []
+        list2_sims = list(cosine_similarity([mean_pooled[i]],mean_pooled[len(list1):])[0])
+        list2_copy = list2.copy()
+        for j in range(3):
+            if len(list2_sims)>0:
+                max_idx = list2_sims.index(max(list2_sims))
+                max_term = list2_copy[max_idx]
+                top_3_matches.append(max_term)
+                max_score = list2_sims[max_idx]
+                top_3_scores.append(max_score)
+                del list2_sims[max_idx]
                 del list2_copy[max_idx]
             else:
-                print('NONE')
                 top_3_matches.append(None)
                 top_3_scores.append(None)
-        df_matches_data.append(top_3_matches)
-        print(top_3_matches)
         df_scores_data.append(top_3_scores)
+        df_matches_data.append(top_3_matches)
 
     df_matches = pd.DataFrame()
     df_matches['list1'] = list1
@@ -75,6 +77,7 @@ def bert_match(list1, list2, type, join_punct = ', '):
     df_scores['2'] = [d[1] for d in df_scores_data]
     df_scores['3'] = [d[2] for d in df_scores_data]
     return df_matches, df_scores
+
 
 list_of_strings1 = ['turkey, cheese, cheddar', 'watermelon, lemon']
 list_of_strings2 = ['turkey, cheese, cheddar', 'ham, lemon, mayo']
--- mapper.py
+++ mapper.py
@@ -2,6 +2,8 @@ from clean import *
 from BERT_match import *
 from fuzzy_match import *
 from tf_idf_match import *
+import numpy as np
+import pandas as pd
 
 def do_map(l1,l2,type = "strings",preference = ""):
     """
@@ -11,14 +13,79 @@ def do_map(l1,l2,type = "strings",preference = ""):
     otherwise state a preferance ("BERT", "fuzzy", or "tf-idf")
     *Requirements: transformers, torch, fuzzywuzzy, sklearn, wordnet
     """
-    if preference == "":
-        fuzzy_df, fuzzy_scores  = fuzzy_match(l1,l1, type = type)
+    def pick_best(df1,df2,df3):
+        final_matches = []
+        df1_vs_df2 = (df1==df2).astype(int)
+        df2_vs_df3 = (df2==df3).astype(int)
+        df3_vs_df1 = (df3==df1).astype(int)
+        total = df1_vs_df2+df2_vs_df3+df3_vs_df1
+        for i in range(len(total)):
+            if total.iloc[i,1] == 3: # if they all agree on top match
+                final_matches.append(df1['1'][i])
+            elif total.iloc[i,1] == 1: # if 2 methods agree on top match
+                if df1_vs_df2.iloc[i,1] == 1:
+                    final_matches.append(df1['1'][i])
+                elif df2_vs_df3.iloc[i,1] == 1:
+                    final_matches.append(df2['1'][i])
+                elif df3_vs_df1.iloc[i,1] == 1:
+                    final_matches.append(df3['1'][i])
+            else: # if no methods agree on the top match take vote from top 3 instead
+                list_1 = [df1.iloc[i,1],df1.iloc[i,2],df1.iloc[i,3]]
+                list_2 = [df2.iloc[i,1],df2.iloc[i,2],df2.iloc[i,3]]
+                list_3 = [df3.iloc[i,1],df3.iloc[i,2],df3.iloc[i,3]]
+                ls = list_1+list_2+list_3
+                unique = np.unique(ls)
+                count_ls = []
+                for u in unique:
+                    count_ls.append(ls.count(u))
+                # if there is a tie, pick the one with the higher ranking in any one of the methods
+                # if it's still a tie, then pick randomly
+                max_count = max(count_ls)
+                if ls.count(max_count) >1:
+                    tie = 0
+                    idxs = [index for (index, item) in enumerate(count_ls) if item == max_count]
+                    top_words = [unique[i] for i in idxs]
+                    score = [0 for word in top_words]
+                    while tie < 3:
+                        for w in range(len(top_words)):
+                            if top_words[w] == list_1[tie]:
+                                score[w] += 1
+                            if top_words[w] == list_2[tie]:
+                                score[w] += 1
+                            if top_words[w] == list_3[tie]:
+                                score[w] += 1
+                        if score.count(max(score)) == 1: # tie has been broken
+                            final_matches.append(top_words[score.index(max(score))])
+                            tie = 5
+                        else:
+                            tie += 1
+                            if tie == 3: # tie hasn't been broken so pick randomly between top scoring words
+                                tw_idx = [index for (index, item) in enumerate(score) if item == max(score)]
+                                final_matches.append(np.random.choice(np.array([top_words[i] for i in tw_idx])))
+
+                else: # no tie to break
+                    final_matches.append(unique[unique.index(max_count)])
+        return final_matches
+
+
+
+
+
+
+
+
+
+    if preference == "": # perform all methods and do a concensus vote
+        fuzzy_df, fuzzy_scores  = fuzzy_match(l1,l2, type = type)
         tf_df, tf_scores = tf_idf_match(l1,l2,type = type)
-
-
+        BERT_df, BERT_scores = bert_match(l1,l2, type = type)
+        matches = pick_best(fuzzy_df,tf_df,BERT_df)
+        df = pd.DataFrame()
+        df['list1'] = tf_df['list1']
+        df['list2 matches'] = matches
+        return df
 
     else:
-
         ls = [bert_match,fuzzy_match,tf_idf_match]
         dictionary = {"BERT":0,"fuzzy":1, "tf-idf":2}
         return ls[dictionary[preference]](l1,l2, type = type)
@@ -39,7 +106,7 @@ list_of_strings2 = [
     "yellow is a color."
 ]
 
-print(do_map(list_of_strings1,list_of_strings2,type = 'strings',preference = 'BERT'))
+print(do_map(list_of_strings1,list_of_strings2,type = 'strings',preference = ''))
 
 
 # print(do_map(list_of_lists1,list_of_lists2,type = 'lists',preference = 'tf-idf'))
